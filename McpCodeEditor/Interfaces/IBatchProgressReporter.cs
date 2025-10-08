b8c67ba11f6ac8dfb637aba7c2fba9eb22afbedb
Michael Jordan <jordan.mymail@gmail.com>
2025-10-07 23:34:30 -0400

    Drop McpCodeEditor

--- McpCodeEditor/Interfaces/IBatchProgressReporter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace McpCodeEditor.Interfaces;
-
-public interface IBatchProgressReporter
-{
-    void ReportProgress(int processed, int total, string? currentFile = null);
-    void ReportFileCompleted(string filePath, bool success, string? error = null);
-}
--- McpCodeEditor/Models/LineChange.cs
+++
@@ -1,9 +0,0 @@
-namespace McpCodeEditor.Models;
-
-public class LineChange
-{
-    public int LineNumber { get; set; }
-    public string Original { get; set; } = string.Empty;
-    public string Modified { get; set; } = string.Empty;
-    public string ChangeType { get; set; } = string.Empty;
-}
